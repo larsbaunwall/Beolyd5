12f50a5534960be373ed99957766d1c137fbe214
Lars Baunwall <larsbaunwall@users.noreply.github.com>
2024-02-07 00:35:04 +0100

    Add HW support for the BS5 wheel in the tauri backend

--- src/ui/src-tauri/src/main.rs
+++ src/ui/src-tauri/src/main.rs
@@ -1,35 +1,55 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-// #[tauri::command]
-// fn greet(name: &str) -> String {
-//     format!("Hello, {}! You've been greeted from Rust!", name)
-// }
-
 #[tauri::command]
 fn tick() {
   println!("Tick");
 }
 
+use std::sync::{Arc, Mutex};
+use beolyd5_controller::{Beolyd5Controller, Button, Wheel};
 use tauri::Manager;
 
 // the payload type must implement `Serialize` and `Clone`.
 #[derive(Clone, serde::Serialize)]
 struct Payload {
-  message: String,
+    message: String,
 }
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![tick])
         .setup(|app| {
-            // emit the `event-name` event to all webview windows on the frontend
-            app.emit_all("navWheelUpdated", ()).unwrap();
+            let app_handle = app.handle();
+            let controller = Arc::new(Mutex::new(Beolyd5Controller::new()));
+            let controller_clone = Arc::clone(&controller);
+
+            let _ = match controller_clone.lock().unwrap().open() {
+                Ok(_) => app_handle.emit_all("deviceOpened", ()),
+                Err(err) => app_handle.emit_all("error", Payload {
+                    message: format!("Failed to open device: {:?}", err),
+                }),
+            };
+
+            let app_handle_clone = app_handle.clone();
+            controller_clone.lock().unwrap().register_wheel_event_callback(Arc::new(Mutex::new(move |(wheel, pos): (Wheel, u8)| {
+                let payload = Payload {
+                    message: format!("Received WheelEvent: {:?} at position {}", wheel, pos),
+                };
+                app_handle_clone.emit_all("wheelEvent", Some(payload)).unwrap();
+            })));
+
+            let app_handle_clone = app_handle.clone();
+            controller_clone.lock().unwrap().register_button_event_callback(Arc::new(Mutex::new(move |button: Button| {
+                let payload = Payload {
+                    message: format!("Received ButtonEvent: {:?}", button),
+                };
+                app_handle_clone.emit_all("buttonEvent", Some(payload)).unwrap();
+            })));
+
             Ok(())
           })
         .run(tauri::generate_context!())
         .expect("error while running BS5 controller UI application");
 }
-
 
