7530e1fdbb8b0d252e844f7c89d04dfd92ef5b4c
Lars Baunwall <larsbaunwall@users.noreply.github.com>
2024-02-07 17:14:18 +0100

    Add thread example

--- src/rust/examples/listen.rs
+++ src/rust/examples/listen.rs
@@ -1,6 +1,7 @@
 extern crate beolyd5_controller;
 
 use std::sync::{Arc, Mutex};
+use std::thread;
 use beolyd5_controller::{Beolyd5Controller, Button, SystemEvent, Wheel};
 
 
@@ -8,33 +9,35 @@ fn main() {
     let controller = Arc::new(Mutex::new(Beolyd5Controller::new()));
     let controller_clone = controller.clone();
 
-    // Register a callback to handle device events
-    controller.lock().unwrap().register_device_event_callback(Arc::new(Mutex::new(move |event: SystemEvent| {
-        println!("** Received SystemEvent: {:?}", event);
+    // Spawn a new thread
+    thread::spawn(move || {
+        // Register a callback to handle device events
+        controller.lock().unwrap().register_device_event_callback(Arc::new(Mutex::new(move |event: SystemEvent| {
+            println!("** Received SystemEvent: {:?}", event);
+        })));
 
-    })));
+        controller.lock().unwrap().register_wheel_event_callback(Arc::new(Mutex::new(move |(wheel, pos): (Wheel, u8)| {
+            println!("   Received WheelEvent: {:?} at position {}", wheel, pos);
+        })));
 
-    controller.lock().unwrap().register_wheel_event_callback(Arc::new(Mutex::new(move |(wheel, pos): (Wheel, u8)| {
-        println!("   Received WheelEvent: {:?} at position {}", wheel, pos);
-    })));
+        controller.lock().unwrap().register_button_event_callback(Arc::new(Mutex::new(move |button: Button| {
+            println!("   Received ButtonEvent: {:?}", button);
 
-    controller.lock().unwrap().register_button_event_callback(Arc::new(Mutex::new(move |button: Button| {
-        println!("   Received ButtonEvent: {:?}", button);
+            if button != Button::None {
+                // Emit click sound
+                controller_clone.lock().unwrap().click().unwrap();
+            }
+        })));
 
-        if button != Button::None {
-            // Emit click sound
-            controller_clone.lock().unwrap().click().unwrap();
+        // Open the device
+        match controller.lock().unwrap().open() {
+            Ok(_) => println!("Device opened successfully"),
+            Err(err) => eprintln!("Failed to open device: {:?}", err),
         }
-    })));
 
-    // Open the device
-    match controller.lock().unwrap().open() {
-        Ok(_) => println!("Device opened successfully"),
-        Err(err) => eprintln!("Failed to open device: {:?}", err),
-    }
-
-    // Keep the main thread alive to continue receiving events
-    loop {
-        std::thread::sleep(std::time::Duration::from_millis(1));
-    }
+        // Keep the main thread alive to continue receiving events
+        loop {
+            std::thread::sleep(std::time::Duration::from_millis(1));
+        }
+    }).join().unwrap();
 }
